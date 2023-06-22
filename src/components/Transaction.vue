673c1960d8a6e064ab5aa194da9b6cdde8971a3c
YQWOH <93274829+YQWOH@users.noreply.github.com>
2023-06-23 00:18:19 +0800

    Add files via upload

---
+++ src/components/Transaction.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="flex items-center">
+    <span :class="`material-icons ${transClass}`">{{ transIcon }}</span>
+    <p>{{ transText }}</p>
+  </div>
+  <div class="flex items-center">
+    <p :style="'margin-right:10px; color: ' + transColor">{{ transType }} {{ transCurrent }}</p>
+    <button :id="transId" class="transaction__bin" @click="deleteTrans($event)">
+      <span class="material-icons">delete</span>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Transaction",
+  props: {
+    transClass: {
+      type: String,
+      default: "",
+    },
+    transIcon: {
+      type: String,
+      default: "",
+    },
+    transColor: {
+      type: String,
+      default: "",
+    },
+    transType: {
+      type: String,
+      default: "",
+    },
+    transCurrent: {
+      type: String,
+      default: "",
+    },
+    transText: {
+      type: String,
+      default: "",
+    },
+    transId: {
+      type: Number,
+      default: null,
+    },
+  },
+  methods: {
+    deleteTrans(e) {
+      console.log("delete");
+      console.log("e: ", e.target.parentElement.id);
+      this.$emit("deleteTransaction", e.target.parentElement.id)
+    },
+  },
+};
+</script>
